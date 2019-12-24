67b692faa190b12f79cb4e828d7e4cf3e6a1e54b
fuhongxue <345166018@qq.com>
2019-12-24 08:56:38 +0800

    一个简单的Jetpack-Navigation示例
    一个简单的Jetpack-Navigation示例

---
+++ HxNavigation/app/src/main/java/com/hongx/navigation/MainPage1Fragment.java
@@ -0,0 +1,30 @@
+package com.hongx.navigation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class MainPage1Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_page1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn = view.findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_page2);
+            }
+        });
+    }
+}
